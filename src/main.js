e11ced549dfa119f60bffcf864c4a66516f1e15b
TinaL_Mac <tina_lin@cmoney.com.tw>
2025-03-16 00:00:55 +0800

    feat: 新增圖表互動方式以及 README.md

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import * as d3 from 'd3';
 // Set dimensions and margins
 const margin = {top: 20, right: 20, bottom: 30, left: 300};
 const width = 800 - margin.left - margin.right;
-const height = 500 - margin.top - margin.bottom;
+const height = 600 - margin.top - margin.bottom;
 
 // Create SVG
 const svg = d3.select("#chart")
@@ -12,6 +12,26 @@ const svg = d3.select("#chart")
   .attr("height", height + margin.top + margin.bottom)
   .append("g")
   .attr("transform", `translate(${margin.left},${margin.top})`);
+
+// Create legend
+const legend = d3.select("#legend")
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", 50)
+  .append("g")
+  .attr("transform", `translate(${(width + margin.left + margin.right) / 2 - 100}, 10)`);
+
+legend.append("rect")
+  .attr("x", 0)
+  .attr("y", 0)
+  .attr("width", 20)
+  .attr("height", 20)
+  .attr("fill", "#1f77b4");
+
+legend.append("text")
+  .attr("x", 30)
+  .attr("y", 15)
+  .text("COSCUP 2024 各開源社群議程數");
 
 // Fetch JSON data
 fetch('https://coscup.org/2024/json/session.json')
@@ -47,7 +67,9 @@ fetch('https://coscup.org/2024/json/session.json')
     // Add y-axis
     svg.append("g")
       .attr("class", "axis")
-      .call(d3.axisLeft(y));
+      .call(d3.axisLeft(y))
+      .selectAll("text")
+      .attr("class", "community-name");
 
     // Add x-axis
     svg.append("g")
@@ -63,10 +85,23 @@ fetch('https://coscup.org/2024/json/session.json')
       .attr("y", d => y(d.type))
       .attr("x", 0)
       .attr("height", y.bandwidth())
-      .attr("width", d => x(d.count));
+      .attr("width", d => x(d.count))
+      .on("mouseover", function(event, d) {
+        d3.select(this).attr("fill", "#ff7f0e");
+        d3.selectAll(".community-name").filter(function(name) {
+          return name === d.type;
+        }).attr("fill", "#ff7f0e");
+      })
+      .on("mouseout", function(event, d) {
+        d3.select(this).attr("fill", "#1f77b4");
+        d3.selectAll(".community-name").filter(function(name) {
+          return name === d.type;
+        }).attr("fill", "#333");
+      });
 
 
-    // Add count labels to the right of the top three bars
+
+    // Add top label to the right of the top three bars
     svg.selectAll(".label")
       .data(typeData.slice(0, 1))
       .enter().append("text")
